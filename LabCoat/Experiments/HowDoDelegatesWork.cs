a8f66b6f692cb1a2fa1743bc28bb4122ca6b693b
Clinton_Avery <usa.pants45@gmail.com>
2020-11-26 08:44:43 -0600

    new experiments

---
+++ LabCoat/Experiments/HowDoDelegatesWork.cs
@@ -0,0 +1,43 @@
+﻿namespace Sandbox.Experiments
+{
+    using System;
+
+    internal class HowDoDelegatesWork : IExperiment
+    {
+        public delegate int Transformer(int x);
+
+        public delegate string Stringer(int x);
+
+        private static int Square(int x)
+        {
+            return x * x;
+        }
+
+        private static string GimmeAnIntIllGiveYaAString(int x)
+        {
+            return x.ToString();
+        }
+
+        public void Experiment()
+        {
+            Transformer t = Square;
+            Console.WriteLine(t(3));
+            Transformer t2 = (a) => 20 + 20;
+            Stringer stringer = GimmeAnIntIllGiveYaAString;
+            Console.WriteLine(nameof(stringer) + " is a " + stringer(44).GetType() + " and it's value is " + stringer(44));
+            Console.WriteLine(t2(4));
+        }
+
+        public string TakeAMethodAsAParameter(Transformer transformer)
+        {
+            int result = transformer(4);
+
+            return result.ToString();
+        }
+
+        public string IdentifyExperiment()
+        {
+            return nameof(HowDoDelegatesWork);
+        }
+    }
+}
---
+++ LabCoat/Experiments/HowLongDoesItTakeToCountAndPrint.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Sandbox.Experiments
+{
+    internal class HowLongDoesItTakeToCountAndPrint : IExperiment
+    {
+        public void Experiment()
+        {
+            for (int i = 0; i < 50000; i++)
+            {
+                Console.Write(i + " ");
+            }
+        }
+
+        public string IdentifyExperiment()
+        {
+            return nameof(HowLongDoesItTakeToCountAndPrint);
+        }
+    }
+}
