547061ae2f8eee798b3318f12eddc3f3579e0e5d
avEggHead <usa.pants45@gmail.com>
2025-07-15 06:06:17 -0400

    final solution for linked list node removal

--- LabCoat/Experiments/LinkedListNodeRemover.cs
+++ LabCoat/Experiments/LinkedListNodeRemover.cs
@@ -1,10 +1,52 @@
-﻿namespace LabCoat.Experiments
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LabCoat.Experiments
 {
     internal class LinkedListNodeRemover : IExperiment
     {
         public void Experiment()
         {
-            throw new System.NotImplementedException();
+            ListNode node = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+            RemoveNthFromEnd(node, 2);
+
+            ListNode newNode = new ListNode(1);
+            var result = RemoveNthFromEnd(newNode, 1);
+
+            Console.WriteLine("done");
+        }
+
+        public ListNode RemoveNthFromEnd(ListNode head, int n)
+        {
+            Console.WriteLine("Count the old list");
+            Console.WriteLine("Head val: " + head.val);
+            ListNode next = head;
+            int counter = 0;
+            List<int> values = new List<int>();
+
+            while (next != null)
+            {
+                Console.WriteLine(next.val);
+                values.Add(next.val);
+                next = next.next;
+                counter++;
+            }
+
+            values.RemoveAt(counter - n);
+            values.Reverse();
+            ListNode newNode = null;
+            ListNode lastOne = null;
+            int index = 0;
+            foreach (int value in values)
+            {
+                ListNode temp = new ListNode(value);
+                temp.next = lastOne ?? null;
+                newNode = temp;
+                lastOne = temp;
+            }
+            
+            return newNode;
         }
 
         public string IdentifyExperiment()
