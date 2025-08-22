4164f754d7ba65008e60cbe976a99ac1e4edfef9
avEggHead <usa.pants45@gmail.com>
2025-08-22 06:56:44 -0400

    needle in haystack

---
+++ LabCoat/Experiments/NeedleInHaystack.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace LabCoat.Experiments
+{
+    internal class NeedleInHaystack : IExperiment
+    {
+        public void Experiment()
+        {
+            int result = this.StrStr("hello", "ll");
+            Console.WriteLine("Expected: 2");
+            Console.WriteLine("Actual: " + result);
+
+            int result2 = this.StrStr("sadbutsad", "sad");
+            Console.WriteLine("Expected: 0");
+            Console.WriteLine("Actual: " + result2);
+        }
+        public int StrStr(string haystack, string needle)
+        {
+            int result = 0;
+            if (!haystack.Contains(needle))
+            {
+                result = -1;
+            }
+            else
+            {
+                result = FindFirstIndexOfMatch(haystack, needle);
+            }
+
+            return result;
+        }
+
+        private int FindFirstIndexOfMatch(string haystack, string needle)
+        {
+            int indexOfNeedle = 0;
+            
+            for(int indexOfHaystack = 0; indexOfHaystack < haystack.Length; indexOfHaystack++)
+            {
+                int tempHaystackIndex = indexOfHaystack;
+                int tempNeedleIndex = 0;
+                bool isFound = false;
+                while (haystack[tempHaystackIndex] == needle[tempNeedleIndex])
+                {
+                    if(tempNeedleIndex == needle.Length - 1)
+                    {
+                        indexOfNeedle = indexOfHaystack;
+                        isFound = true;
+                        break;
+                    }
+                    else
+                    {
+                        tempNeedleIndex++;
+                        tempHaystackIndex++;
+                    }
+                };
+                if (isFound) break;
+            }
+
+            return indexOfNeedle;
+        }
+
+        public string IdentifyExperiment()
+        {
+            return "NeedleInHaystack";
+        }
+    }
+}
---
+++ LabCoat/Experiments/RemoveOccurencesOfInteger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace LabCoat.Experiments
+{
+    internal class RemoveOccurencesOfInteger : IExperiment
+    {
+        public void Experiment()
+        {
+            throw new System.NotImplementedException();
+        }
+        public int RemoveElement(int[] nums, int val)
+        {
+            List<int> numbers = new List<int>();
+
+            foreach (int number in nums)
+            {
+                if(number != val) numbers.Add(number);
+            }
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                nums[i] = numbers[i];
+            }
+
+            return numbers.Count;
+        }
+
+        public string IdentifyExperiment()
+        {
+            return "RemoveOccurencesOfInteger";
+        }
+    }
+}
